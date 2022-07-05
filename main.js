0a2dda6ee7993fa162c1844710295914faea1464
gstreluk <guillestreluk@gmail.com>
2022-07-04 23:16:37 -0300

    java

--- main.js
+++ main.js
@@ -1,73 +1,93 @@
 // Tienda Web
 
-function bienvenida() {
-    alert("Bienvenido a la tienda Web");
+let nombre = prompt ("ingrese su nombre");
+
+alert ("Hola"+" "+nombre+" "+"bienvenido a la tienda web");
+
+ 
+
+let entrada2 = prompt("Te gusto algun producto?");
+
+if ((entrada2 == "si") || (entrada2 == "SI")) {
+
+    alert("Perfecto, te ayudaremos a elegir correctamente");
+
+} else {
+
+    alert("sigue disfrutando de nuestro contenido");
+
 }
 
-function comprar () {
-    producto = prompt(
-      "Elegí el producto que deseas comprar: \n 1: Micrófono $ 7.000 \n 2: Panel acústico $ 4.500 \n 3: Kit de grabación $ 15.000\n 4: Auriculares $ 5.500\n 5: Volver"
-    );
-      if (producto === "1") {
-      alert(" Elegiste Micrófono");
-    } else if (producto === "2") {
-      alert(" Elegiste Panel acústico");
-    } else if (producto === "3") {
-      alert(" Elegiste Kit de grabación");
-    } else if (producto === "4") {
-      alert(" Elegiste Auriculares");
+ 
+
+const productos = [];
+
+class Productos {
+
+    constructor (codigo,producto, precio) {
+
+this.producto = producto;
+
+this.precio =precio;
+
+this.codigo = codigo;
+
     }
-    else if (producto === "5") {
-        inicio();
-    }
-    totalCompra();
-  }
-  
-function inicio() {
-    let opcion = parseInt(
-      prompt(
-        "Ingrese :\n1)Comenzar a comprar \n2)Recibir ofertas por mail \n3)Salir"
-        )
-    );
-    while (opcion !== 3) {
-      switch (opcion) {
-        case 1:
-          comprar();
-          break;
-        case 2:
-          ofertas();
-          break;
-      }
-      opcion = parseInt(
-        prompt(
-        "Ingrese :\n1)Comenzar a comprar \n2)Recibir ofertas por mail \n3)Salir"
-        )
-      );
-    }
-  }
-  function totalCompra(mi_array, funcion) {
-    for (const indice of numeros) {
-        funcion(indice);
-    }
-  }
-  //let compras1=parseFloat(prompt("ingrese el monto"));
-  //let compras2=parseFloat(prompt("ingrese el segundo monto"));
-  function acumular(num) {
-      total += num;
-  }
-  
-  function ofertas (){
-    let mail = prompt("Ingrese el mail donde desea recibir las novedades con los mejores precios")
-    alert ("Informacion enviada al mail:  " + mail)
+
+}
+
+let cantidad =parseInt(prompt("Cuantos productos quieres agregar?"))
+
+ 
+
+function agregarproductos (arr,cantidad) {
+
+  for (let i = 0; i < cantidad; i++) {
+
+   let producto=prompt("ingrese el producto")
+
+   let precio=parseInt(prompt("ingrese el precio"))
+
+   let codigo = prompt("ingrese el codigo")
+
+   arr.push(new Productos (codigo,producto,precio))
+
   }
 
-let producto;
-bienvenida();
-inicio();
-totalCompra(numeros, acumular);
-  console.log("Mi total es: " + total);
+}
 
-  const numeros= [producto+producto]
-totalCompra(numeros, console.log)
-let total = 0;  
+function mostrarproductos(arr){
+
+    arr.forEach(item => {
+
+        console.log ("Producto: "+ item.producto+" Precio: "+item.precio+ " Codigo: "+ item.codigo)
+
+    });
+
+}
+ 
+
+agregarproductos(productos,cantidad)
+
+mostrarproductos (productos)
+
+ 
+
+let operacion = prompt ("Ingrese el codigo del producto seleccionado")
+
+ 
+
+let productoselegidos = productos.filter((item)=> item.codigo == operacion)
+
+let mensaje = ""
+
+productoselegidos.forEach(item => {
+
+    mensaje += "Producto: "+ item.producto + "Precio: "+item.precio + "Codigo: "+item.codigo
+
+});
+
+ 
+
+alert(mensaje)
 
