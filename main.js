d04224d5eb74b418639855d85678ed7ee67f36ec
gstreluk <guillestreluk@gmail.com>
2022-07-18 01:28:28 -0300

    carrito

--- main.js
+++ main.js
@@ -91,33 +91,142 @@ productoselegidos.forEach(item => {
 
 //alert(mensaje)
 
+const productos = [
+                    {imagen:"microfono.jpg" , id:1, nombre: "Micrófono HIGH ELECTRIC CONDERSER", precio: 7.000, codigo: "Cod. 101", },
+                    {imagen:"panel_acustico.jpg" , id:2, nombre: "Cabina De Grabacion Panel Acustico 5 Tramos Portatil Estudio", precio: "4.500", codigo:"Cod. 102" },
+                    {imagen:"MICRO_inalambrico.jpg" , id:3, nombre: "Microfonos inalambricos", precio: 11.000, codigo:"Cod. 103" },
+                    {imagen:"kit_de_grabacion.jpg" , id:4, nombre: "Kit Microfono Profesional Condenser Con Brazo + Phanton", precio: 15.000, codigo:"Cod. 104" },
+                    {imagen:"auriculares.jpg" , id:5, nombre: "Auriculares inalámbricos Sony WH-CH510 negro", precio: 5.500, codigo:"Cod. 105" },
+                    {imagen:"libro.jpg" , id:6, nombre: "Libro - Aula De Locución de Blanch, Margarita / Lázaro, Patrícia", precio: 3.500, codigo:"Cod. 106 " }
+
+                ]
+ 
+function obtenerArticulosLS(){
+    return JSON.parse(localStorage.getItem("articulos"))|| [];
+}
+function guardarArticulosLS(){
+    localStorage.setItem("articulos", JSON.stringify(productos));
+}
+function obtenerArticulosCarrito(){
+    return JSON.parse(localStorage.getItem("carrito"))|| [];
+}
+function guardarArticulosCarrito(){
+    localStorage.setItem("carrito", JSON.stringify(productos));
+}
+
+
+
+
+
+function mostrarArticulos(){
+        let productos= obtenerArticulosLS();
+        let contenido = "";
+
+        for (const producto of productos){
+            contenido += `<img src=../assets/img/${producto.imagen}><br>
+            <h5 >${producto.id}</h5>
+            <p >${producto.nombre}</p>
+            <p>$${producto.precio}</p>
+            <p>${producto.codigo}</p>
+            <button type="button" class="btn btn-primary" id=${producto.id}" >Comprar</button>`;
+        }
+        document.getElementById("articulos").innerHTML = contenido;
+    }
+
+function actualizarBotonCarrito(){
+    let productos= obtenerArticulosCarrito();
+    let contenido =`<button type="button" class="btn btn-primary position-relative">
+    Carrito
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+      0</span></button>`;
+    let total = 0;
+
+     if(productos.lenght > 0) {
+        for (let producto of productos){
+            total += producto.cantidad;
+        }
+        contenido =`<button type="button" class="btn btn-primary position-relative">
+    Carrito
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+      ${total}</span></button>`;
+    }
+
+    document.getElementById("boton_carrito").innerHTML = contenido;
+}
+function buscarProducto(id){
+    let productos= obtenerArticulosLS();
+    return productos.find(x => x.id ==id)
+}
+function agregarCarrito(id){
+    let producto = buscarProducto(id);
+    let productos_carrito = obtenerArticulosCarrito();
+    producto.cantidad = 1;
+    productos_carrito.push(producto);
+    guardarArticulosCarrito(productos_carrito);
+    actualizarBotonCarrito();
+
+}
+
+guardarArticulosLS(productos);
+actualizarBotonCarrito();
+mostrarArticulos();
+
+
+
+/*
+ for (const articulo of articulos){
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML= `
+                        <img src=../assets/img/${articulo.imagen}><br>
+                        <h5 >${articulo.id}</h5>
+                        <p >${articulo.nombre}</p>
+                        <p>$${articulo.precio}</p>
+                        <p>${articulo.codigo}</p>
+                        <button type="button" class="btn btn-primary" id=${articulo.id} >Comprar</button>`;
+ document.getElementById("articulos").appendChild(contenedor);
+}
+
+
 let compras = document.getElementById("productos")
 compras[0].innerText = "Microfonos"
 compras[1].innerText = "Auriculares"
 compras[2].innerText = "Otros"
-
-let boton1 = document.getElementById("micro")
+*/
+let boton1 = document.getElementById("1")
       boton1.addEventListener("click", respuestaClick1)
       function respuestaClick1(){
         let compras = document.getElementById("compras")
-        compras.innerText ="Micrófono HIGH ELECTRIC CONDERSER \n $ 7.000 \nCod.270" 
+        compras.innerHTML = "Micrófono HIGH ELECTRIC CONDERSER \n $ 7.000 \nCod.270" 
       }
 
-let boton2 = document.getElementById("panel")
+let boton2 = document.getElementById("2")
 boton2.addEventListener("click", respuestaClick2)
 function respuestaClick2() {
     let compras = document.getElementById("compras")
     compras.innerText ="Cabina De Grabacion Panel Acustico 5 Tramos Portatil \n $ 4.500 \nCod.150" 
 }
-let boton3 = document.getElementById("kit")
+let boton3 = document.getElementById("3")
 boton3.addEventListener("click", respuestaClick3)
 function respuestaClick3(){
     let compras = document.getElementById("compras")
-    compras.innerText ="Kit Microfono Profesional Condenser Con Brazo + Phanton 20hz \n $ 15.000 \nCod.203" 
+    compras.innerText ="Kit inalambrico \n $ 11.000 \nCod.203" 
 }
-let boton4 = document.getElementById("auriculares")
-boton4.addEventListener("click", respuestaClick4)
+
+let boton4 = document.getElementById("4")
+boton3.addEventListener("click", respuestaClick4)
 function respuestaClick4(){
     let compras = document.getElementById("compras")
+    compras.innerText ="Kit Microfono Profesional Condenser Con Brazo + Phanton 20hz \n $ 15.000 \nCod.203" 
+}
+let boton5 = document.getElementById("5")
+boton4.addEventListener("click", respuestaClick5)
+function respuestaClick5(){
+    let compras = document.getElementById("compras")
     compras.innerText ="Auriculares inalámbricos Sony WH-CH510 negro \n $ 5.500 \nCod.150" 
+}
+let boton6 = document.getElementById("6")
+boton3.addEventListener("click", respuestaClick6)
+function respuestaClick6(){
+    let compras = document.getElementById("compras")
+    compras.innerText ="Libro \n $ 15.000 \nCod.203" 
 }
